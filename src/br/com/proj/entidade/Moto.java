a474ce6465e409eec882da04dcc9a0308a0c66ec
Aluno Fiar Logatti <aluno@fiar.local>
2018-02-21 21:52:57 -0300

    camada de dados

--- src/br/com/proj/entidade/Moto.java
+++ src/br/com/proj/entidade/Moto.java
@@ -1,5 +1,29 @@
 package br.com.proj.entidade;
 
 public class Moto {
+private int  id;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+private String descricao;
+private int ano;
+
+
+public String getDescricao() {
+	return descricao;
+}
+public void setDescricao(String descricao) {
+	this.descricao = descricao;
+}
+public int getAno() {
+	return ano;
+}
+public void setAno(int ano) {
+	this.ano = ano;
+}
+
 
 }
